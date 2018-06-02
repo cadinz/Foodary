aff2dd53c0a7e57403b204d0c53fddff6773caee
오세진 <osejin@osejin-ui-MacBook-Pro.local>
2018-06-03 01:03:18 +0900

    add Toast

--- app/src/main/java/com/dongyang/dev/seoungik/MainActivity.java
+++ app/src/main/java/com/dongyang/dev/seoungik/MainActivity.java
@@ -2,12 +2,14 @@ package com.dongyang.dev.seoungik;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(this, "수정된 결과", Toast.LENGTH_SHORT).show( );
         setContentView(R.layout.activity_main);
     }
 }
